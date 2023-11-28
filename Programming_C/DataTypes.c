04f976fd99faf01d876473f8c78d9992e3503a51
mjsaikot <mjsaikot@gmail.com>
2023-11-29 02:28:09 +0600

    Start problem solving by programming C Today date:29/11/2023

---
+++ Programming_C/DataTypes.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main() {
+    // Write C code here
+      int a = 10;
+      char b = 'B';
+      char s = "Bangladesh";
+      printf("Integer: %d", a);
+      printf("Character: %c", b);
+      printf("String: %s", s);
+      if(a > 12)
+      {
+        printf("true");
+      }
+      else{
+        printf("False");
+      }
+    
+    return 0;
+}
